4382bae233722dd2600ecd63e0e3bc49bde178a1
Sandesh Shirse <shirse170906@gmail.com>
2025-12-31 10:37:48 +0530

    Add files via upload

---
+++ practiceJavaScript/prizeCalculator.js
@@ -0,0 +1,17 @@
+console.log("prize calculator");
+let a= "milk"
+let b= "banana"
+let c= "coffee"
+let item = prompt("enter your item")
+if (item == a){
+    console.log("60.00Rs");
+}
+else if (item == b){
+    console.log("70.00Rs");
+}
+else if (item==c){
+    console.log("10.00Rs");
+}
+else{
+    console.log("dont know");
+}
